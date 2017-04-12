cb383aa27531646e58deaa59d2b5c8e66fed98a2
fred <menty44@gmail.com>
2017-04-12 15:02:24 +0300

    wed v1

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -102,17 +102,22 @@ revolutiontechnologies, revtech, rev tech" />
         <h2 class="w3ls_head">Contact Us</h2>
         <p class="w3layouts_para">Feel free to write to us. Request for a quote or enquire about our products and services or just say JAMBO to us. You are welcomed.</p>
         <div class="contact-right-w3-agile">
-            <form action="#" method="post">
-                <input type="text" name="Name" placeholder="Name" required="">
-                <input type="text" name="Number" placeholder="Phone" required="">
-                <textarea name="Comments" placeholder="Message" required=""></textarea>
+            <p name="enq" id="contact_form" method="post" action="email/" onsubmit="return validation();">
+
+                <input type="text" name="name" placeholder="Name" required="">
+                <input type="text" name="email" placeholder="Email" required="">
+                <input type="text" name="subject" placeholder="Subject" required="">
+                <input type="text" name="company" placeholder="Company" required="">
+                <textarea name="message" placeholder="Message" required=""></textarea>
+
                 <input type="submit" value="Submit">
             </form>
         </div>
     </div>
 </div>
 <div class="map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d805184.6315353649!2d144.49269948060635!3d-37.97123704486416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad646b5d2ba4df7%3A0x4045675218ccd90!2sMelbourne+VIC%2C+Australia!5e0!3m2!1sen!2sin!4v1475562983428" allowfullscreen></iframe>
+    {{--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d805184.6315353649!2d144.49269948060635!3d-37.97123704486416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad646b5d2ba4df7%3A0x4045675218ccd90!2sMelbourne+VIC%2C+Australia!5e0!3m2!1sen!2sin!4v1475562983428" allowfullscreen></iframe>--}}
+    <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyD8aUJQTpqolWkSn6-WYT2mIevdvcf4oOw&q=West+Business+center,Kaunda+Street"  allowfullscreen></iframe>
 </div>
 <!-- //contact -->
 
--- resources/views/email/index.php
+++ resources/views/email/index.php
@@ -1,7 +1,49 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: fred
  * Date: 4/12/17
  * Time: 8:52 AM
  */
+
+
+ if(isset($_POST['submit']))
+ {
+     $name = $_POST['name'];
+     $email = $_POST['email'];
+     $subject = $_POST['SUBJECT'];
+     $company = $_POST['company'];
+     $query = $_POST['message'];
+     $email_from = $name.'<'.$email.'>';
+
+     $to="fred.oluoch@blaqueyard.com";
+     $subject="Revolution Technologies Enquiry!";
+     $headers  = 'MIME-Version: 1.0' . "\r\n";
+     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+     $headers .= "From: ".$email_from."\r\n";
+     $message="	   
+ 	  <br></p> 
+ 		<b>Name :</b>
+		$name 	
+		    
+         <br></p>
+ 		<b>Email-Id :</b>
+		$email         
+       	<br></p>
+ 		<b>Subject :</b>
+		$subject
+       	<br></p>
+ 		<b>Company :</b>
+		$company		
+      	<br></p>
+       		 <b>Message:</b>
+		 $query       
+   ";
+     if(mail($to,$subject,$message,$headers))
+         header("Location:./contact.php?msg=Successful Submission! Thank you for contacting us. Revolution Team");
+     else
+         header("Location:./contact.php?msg=Error To send Email !");
+     echo "contact:fred.oluoch@blaqueyard.com";
+ }
+?>
